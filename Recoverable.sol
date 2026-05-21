2aeb7ed5f42c8f67094518fcf406d10859ebce88
terry661317 <saadmuhammed830@gmail.com>
2026-05-22 06:33:53 +0900

    init

---
+++ Recoverable.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.4.23;
+
+import "../zeppelin/contracts/ownership/CanReclaimToken.sol";
+import "../zeppelin/contracts/ownership/Claimable.sol";
+
+
+/**
+ * @title Recoverable ensures ether and ERC20 tokens can be claimed by the
+ * owner of the contract.
+ * @author Zachary Kilgore @ Xelora Technologies LLC
+ * @dev Prevents accidental loss of tokens and ether erroneously sent to
+ * this contract
+ */
+contract Recoverable is CanReclaimToken, Claimable {
+  using SafeERC20 for ERC20Basic;
+
+  /**
+   * @dev Transfer all ether held by the contract to the contract owner.
+   */
+  function reclaimEther() external onlyOwner {
+    owner.transfer(address(this).balance);
+  }
+
+}
